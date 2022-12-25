63dcd707597c5d2d1fb029b419139d8c724930e8
marcs-feh <marcosfehlauer@protonmail.com>
2022-12-25 11:09:54 -0300

    init

---
+++ test.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#define print(x) std::cout << x << '\n'
+
+int main(int argc, const char** argv){
+	return 0;
+}
---
+++ types.hh
@@ -0,0 +1,25 @@
+#ifndef INCLUDE_MF_TYPES_HH_
+#define INCLUDE_MF_TYPES_HH_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdalign.h>
+
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
+
+using usize   = size_t;
+using uintptr = uintptr_t;
+using byte    = unsigned char;
+
+#endif /* header guard */
